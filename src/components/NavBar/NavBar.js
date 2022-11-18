1b0f41108c3ab863d9e7d8851ca421e00fef3899
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-17 21:05:52 -0300

    1

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <>
+      <div className='navbar'>
+        <ul className='categorias'>
+          <li>Categoria</li>
+          <li>Categoria2</li>
+          <li>Categoria3</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default NavBar
