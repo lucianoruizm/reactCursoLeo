90681df01d682cdd1de3f2c55b2de6fe6073afbf
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-18 23:58:54 -0300

    2

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import { BsCartFill } from 'react-icons/bs';
+
+const CartWidget = () => {
+  return (
+    <div>
+        <BsCartFill />
+        3
+    </div>
+  )
+}
+
+export default CartWidget
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,14 @@
+
+const ItemListContainer = (props) => {
+  return (
+    <ul>
+        <li>{props.name}</li>
+        <li>item 2</li>
+        <li>item 3</li>
+        <li>item 4</li>
+        <li>item 5</li>
+    </ul>
+  )
+}
+
+export default ItemListContainer;
