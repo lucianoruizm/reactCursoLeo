d38c7de1e93e327ba88630105a7db6ec7435b4e1
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-22 12:51:13 -0300

    greeting string

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting={'Greeting'} />
+      <ItemListContainer greeting={'Hello World!'} />
     </div>
   );
 }
